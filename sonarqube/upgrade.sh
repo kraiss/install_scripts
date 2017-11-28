ce4fdf8ba59bd59fd2794003a2b610682aae231e
Pierrick Rassat <rassat.pierrick@gmail.com>
2017-11-28 17:21:41 +0100

    Update upgrade.sh

--- sonarqube/upgrade.sh
+++ sonarqube/upgrade.sh
@@ -5,6 +5,7 @@
 ## If you are asked to use maintainer jenkins property file,
 ##   refuse and keep your own file to avoid re-configuration (usually 'N')
 sudo apt-get install --only-upgrade sonar
+sudo service sonar restart
 
 ## If the machine does not provide X consider accessing this url from 
 ##   another workspace (http://<sonar_ip_port_and_context>/setup
