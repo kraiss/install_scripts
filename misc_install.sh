68263111f58946638ebcf4db6402e5b5d11d3eab
Pierrick Rassat <rassat.pierrick@gmail.com>
2017-12-19 10:49:49 +0100

    J9 bro !!

--- misc_install.sh
+++ misc_install.sh
@@ -4,7 +4,7 @@
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java9-installer
 
 ## Install Maven 3
 sudo apt-get purge maven maven2 maven3
