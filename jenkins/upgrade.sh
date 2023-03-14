a3afff8fe5329e3f7cdd5203ddbab810314cb51c
Pierrick Rassat <rassat.pierrick@gmail.com>
2023-03-14 11:34:13 +0100

    Delete jenkins directory

--- jenkins/upgrade.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## https://wiki.jenkins.io/display/JENKINS/Installing+Jenkins+on+Ubuntu
-
-## If you are asked to use maintainer jenkins property file,
-##   refuse and keep your own file to avoid re-configuration (usually 'N')
-sudo apt-get install --only-upgrade jenkins
